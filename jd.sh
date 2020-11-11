8c306091f5f3bd84309633ffd315355082e1c5a1
ITdesk01 <38835844+ITdesk01@users.noreply.github.com>
2020-11-11 09:09:45 +0800

    调整js存放位置

--- jd.sh
+++ jd.sh
@@ -9,6 +9,7 @@ version="1.5"
 CRON_FILE=/etc/crontabs/root
 url=https://raw.githubusercontent.com/lxk0301/jd_scripts/master
 dir_file=/usr/share/JD_Script
+dir_file_js=/usr/share/JD_Script/js
 node=/usr/bin/node
 red="\033[31m"
 green="\033[32m"
@@ -17,36 +18,34 @@ white="\033[0m"
 
 
 
-
-
 update() {
-wget $url/jd_xtg.js -O $dir_file/jd_xtg.js 
-wget $url/jd_818.js -O $dir_file/jd_818.js 
-wget $url/jd_superMarket.js -O $dir_file/jd_superMarket.js 
-wget $url/jdSuperMarketShareCodes.js -O $dir_file/jdSuperMarketShareCodes.js 
-wget $url/jd_blueCoin.js -O $dir_file/jd_blueCoin.js 
-wget $url/jd_redPacket.js -O $dir_file/jd_redPacket.js 
-wget $url/jd_moneyTree.js -O $dir_file/jd_moneyTree.js 
-wget $url/jd_fruit.js -O $dir_file/jd_fruit.js 
-wget $url/jdFruitShareCodes.js -O $dir_file/jdFruitShareCodes.js 
-wget $url/jd_pet.js -O $dir_file/jd_pet.js 
-wget $url/jdPetShareCodes.js -O $dir_file/jdPetShareCodes.js 
-wget $url/jd_plantBean.js -O $dir_file/jd_plantBean.js 
-wget $url/jdPlantBeanShareCodes.js -O $dir_file/jdPlantBeanShareCodes.js 
-wget $url/jd_shop.js -O $dir_file/jd_shop.js 
-wget $url/jd_joy.js -O $dir_file/jd_joy.js 
-wget $url/jd_joy_steal.js -O $dir_file/jd_joy_steal.js 
-wget $url/jd_joy_feedPets.js -O $dir_file/jd_joy_feedPets.js 
-wget $url/jd_joy_reward.js -O $dir_file/jd_joy_reward.js 
-wget $url/jd_club_lottery.js -O $dir_file/jd_club_lottery.js 
-wget $url/jd_unsubscribe.js -O $dir_file/jd_unsubscribe.js 
-wget $url/jd_collectProduceScore.js -O $dir_file/jd_collectProduceScore.js
-wget $url/jd_lotteryMachine.js -O $dir_file/jd_lotteryMachine.js
-wget $url/jd_rankingList.js -O $dir_file/jd_rankingList.js
-wget $url/jd_speed.js -O $dir_file/jd_speed.js
-#wget $url/jd_dreamFactory.js -O $dir_file/jd_dreamFactory.js 京东京喜工厂未完成
-sed -i "s/|| 0/|| 20/g" $dir_file/jd_blueCoin.js
-sed -i "s/|| 20/|| 50/g" $dir_file/jd_unsubscribe.js
+wget $url/jd_xtg.js -O $dir_file_js_js/jd_xtg.js 
+wget $url/jd_818.js -O $dir_file_js_js/jd_818.js 
+wget $url/jd_superMarket.js -O $dir_file_js/jd_superMarket.js 
+wget $url/jdSuperMarketShareCodes.js -O $dir_file_js/jdSuperMarketShareCodes.js 
+wget $url/jd_blueCoin.js -O $dir_file_js/jd_blueCoin.js 
+wget $url/jd_redPacket.js -O $dir_file_js/jd_redPacket.js 
+wget $url/jd_moneyTree.js -O $dir_file_js/jd_moneyTree.js 
+wget $url/jd_fruit.js -O $dir_file_js/jd_fruit.js 
+wget $url/jdFruitShareCodes.js -O $dir_file_js/jdFruitShareCodes.js 
+wget $url/jd_pet.js -O $dir_file_js/jd_pet.js 
+wget $url/jdPetShareCodes.js -O $dir_file_js/jdPetShareCodes.js 
+wget $url/jd_plantBean.js -O $dir_file_js/jd_plantBean.js 
+wget $url/jdPlantBeanShareCodes.js -O $dir_file_js/jdPlantBeanShareCodes.js 
+wget $url/jd_shop.js -O $dir_file_js/jd_shop.js 
+wget $url/jd_joy.js -O $dir_file_js/jd_joy.js 
+wget $url/jd_joy_steal.js -O $dir_file_js/jd_joy_steal.js 
+wget $url/jd_joy_feedPets.js -O $dir_file_js/jd_joy_feedPets.js 
+wget $url/jd_joy_reward.js -O $dir_file_js/jd_joy_reward.js 
+wget $url/jd_club_lottery.js -O $dir_file_js/jd_club_lottery.js 
+wget $url/jd_unsubscribe.js -O $dir_file_js/jd_unsubscribe.js 
+wget $url/jd_collectProduceScore.js -O $dir_file_js/jd_collectProduceScore.js
+wget $url/jd_lotteryMachine.js -O $dir_file_js/jd_lotteryMachine.js
+wget $url/jd_rankingList.js -O $dir_file_js/jd_rankingList.js
+wget $url/jd_speed.js -O $dir_file_js/jd_speed.js
+#wget $url/jd_dreamFactory.js -O $dir_file_js/jd_dreamFactory.js 京东京喜工厂未完成
+sed -i "s/|| 0/|| 20/g" $dir_file_js/jd_blueCoin.js
+sed -i "s/|| 20/|| 50/g" $dir_file_js/jd_unsubscribe.js
 }
 
 update_script() {
@@ -58,47 +57,47 @@ echo -e "$green脚本结束，当前时间：`date "+%Y-%m-%d %H:%M"`$white"
 
 run_0() {
 echo "开始运行脚本，当前时间：`date "+%Y-%m-%d %H:%M"`"
-$node $dir_file/jd_xtg.js #星推官0点开搞
-$node $dir_file/jd_redPacket.js #京东全民开红包，没时间要求
-$node $dir_file/jd_moneyTree.js #京东摇钱树，没时间要求
-$node $dir_file/jd_club_lottery.js #摇京豆，没时间要求
-$node $dir_file/jd_lotteryMachine.js #京东抽奖机
-$node $dir_file/jd_rankingList.js #京东排行榜签到领京豆
-$node $dir_file/jd_blueCoin.js #京小超兑换，有次数限制，没时间要求
-$node $dir_file/jd_joy_reward.js #宠汪汪积分兑换奖品，有次数限制，每日京豆库存会在0:00、8:00、16:00更新，经测试发现中午12:00也会有补发京豆
+$node $dir_file_js/jd_xtg.js #星推官0点开搞
+$node $dir_file_js/jd_redPacket.js #京东全民开红包，没时间要求
+$node $dir_file_js/jd_moneyTree.js #京东摇钱树，没时间要求
+$node $dir_file_js/jd_club_lottery.js #摇京豆，没时间要求
+$node $dir_file_js/jd_lotteryMachine.js #京东抽奖机
+$node $dir_file_js/jd_rankingList.js #京东排行榜签到领京豆
+$node $dir_file_js/jd_blueCoin.js #京小超兑换，有次数限制，没时间要求
+$node $dir_file_js/jd_joy_reward.js #宠汪汪积分兑换奖品，有次数限制，每日京豆库存会在0:00、8:00、16:00更新，经测试发现中午12:00也会有补发京豆
 run_10_15_20
 run_06_18
 run_01
 run_03
-$node $dir_file/jd_unsubscribe.js #取关店铺，没时间要求
+$node $dir_file_js/jd_unsubscribe.js #取关店铺，没时间要求
 echo -e "$green脚本结束，当前时间：`date "+%Y-%m-%d %H:%M"`$white"
 }
 
 run_01() {
 echo "开始运行脚本，当前时间：`date "+%Y-%m-%d %H:%M"`"
-$node $dir_file/jd_joy_feedPets.js #宠汪汪喂食一个小时喂一次
-$node $dir_file/jd_collectProduceScore.js #京东全民营业一个小时领金币
-#$node $dir_file/jd_dreamFactory.js 京东京喜工厂未完成
+$node $dir_file_js/jd_joy_feedPets.js #宠汪汪喂食一个小时喂一次
+$node $dir_file_js/jd_collectProduceScore.js #京东全民营业一个小时领金币
+#$node $dir_file_js/jd_dreamFactory.js 京东京喜工厂未完成
 echo "脚本结束，当前时间：`date "+%Y-%m-%d %H:%M"`"
 }
 
 run_03() {
-$node $dir_file/jd_plantBean.js #种豆得豆，没时间要求，三个小时收一次瓶子
+$node $dir_file_js/jd_plantBean.js #种豆得豆，没时间要求，三个小时收一次瓶子
 }
 
 run_06_18() {
 echo "开始运行脚本，当前时间：`date "+%Y-%m-%d %H:%M"`"
-$node $dir_file/jd_818.js #手机商城 每天0/6/12/18
-$node $dir_file/jd_fruit.js #东东水果，没时间要求，一天可以执行两次
-$node $dir_file/jd_shop.js #进店领豆，早点领，一天也可以执行两次以上
-$node $dir_file/jd_joy.js #jd宠汪汪，零点开始，一天也可以执行两次以上
-$node $dir_file/jd_pet.js #东东萌宠，跟手机商城同一时间
-$node $dir_file/jd_joy_steal.js #可偷好友积分，零点开始，六点再偷一波狗粮
+$node $dir_file_js/jd_818.js #手机商城 每天0/6/12/18
+$node $dir_file_js/jd_fruit.js #东东水果，没时间要求，一天可以执行两次
+$node $dir_file_js/jd_shop.js #进店领豆，早点领，一天也可以执行两次以上
+$node $dir_file_js/jd_joy.js #jd宠汪汪，零点开始，一天也可以执行两次以上
+$node $dir_file_js/jd_pet.js #东东萌宠，跟手机商城同一时间
+$node $dir_file_js/jd_joy_steal.js #可偷好友积分，零点开始，六点再偷一波狗粮
 echo "脚本结束，当前时间：`date "+%Y-%m-%d %H:%M"`"
 }
 
 run_10_15_20() {
-$node $dir_file/jd_superMarket.js #京小超,0 10 15 20四场补货加劵
+$node $dir_file_js/jd_superMarket.js #京小超,0 10 15 20四场补货加劵
 }
 
 help() {
@@ -144,6 +143,11 @@ echo ----------------------------------------------------
 description_if() {
 	if [[ ! -d "$dir_file" ]]; then
 		cp -r $(pwd)  $dir_file && chmod 777 $dir_file/jd.sh
+	fi
+	
+	if [[ ! -d "$dir_file/js" ]]; then
+		mdkir $dir_file/js
+		update
 	fi
 	
 	if [[ -f "/usr/share/JD_Script/jdCookie.js" ]]; then
