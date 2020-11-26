2a3862681ec8a0a9093f647d465394b727f918a2
Zhenjian Zhang <itdesk.zhang@gmail.com>
2020-11-26 08:56:08 +0800

    修改京喜工厂
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -162,6 +162,14 @@ additional_settings() {
 	sed -i "s/$old_plantBean4/$new_plantBean_set/g" $dir_file_js/jdPlantBeanShareCodes.js
 	sed -i "s/$.isNode() ? 20 : 5/0/g" $dir_file_js/jd_plantBean.js
 
+	#京喜工厂
+	old_dreamFactory="gB99tYLjvPcEFloDgamoBw=="
+	old_dreamFactory1="V5LkjP4WRyjeCKR9VRwcRX0bBuTz7MEK0-E99EJ7u0k="
+	new_dreamFactory="4HL35B_v85-TsEGQbQTfFg=="
+	new_jdreamFactory1="q3X6tiRYVGYuAO4OD1-Fcg=="
+	sed -i "s/$old_dreamFactory/$new_dreamFactory/g" $dir_file_js/jd_dreamFactory.js
+	sed -i "s/$old_dreamFactory1/$new_dreamFactory1/g" $dir_file_js/jd_dreamFactory.js
+
 	#东东工厂
 	old_jdfactory="P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj"
 	old_jdfactory1="P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo"
