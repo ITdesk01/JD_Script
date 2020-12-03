d58c787dae0415300e7237a9bfdfa8b5d0e9d08f
Zhenjian Zhang <itdesk.zhang@gmail.com>
2020-12-03 10:50:49 +0800

    调整金牌厂长
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -102,7 +102,8 @@ update() {
 	wget $url/jdFactoryShareCodes.js -O $dir_file_js/jdFactoryShareCodes.js
 	wget $url/jd_syj.js -O $dir_file_js/jd_syj.js
 	wget $url/jd_bean_sign.js -O $dir_file_js/jd_bean_sign.js
-	wget $url/jd_bean_sign.js -O $dir_file_js/jd_jxstory.js
+	wget $url/jd_jxstory.js -O $dir_file_js/jd_jxstory.js
+	wget $url/jdJxStoryShareCodes.js -O $dir_file_js/jdJxStoryShareCodes.js
 	wget https://raw.githubusercontent.com/799953468/Quantumult-X/master/Scripts/JD/jd_paopao.js -O $dir_file_js/jd_paopao.js
 	rm -rf $dir_file_js/jx_story.js
 	additional_settings
@@ -234,6 +235,17 @@ deng_20201120_pb="e7lhibzb3zek3knwnjhrbaadekphavflo22jqii@olmijoxgmjutzfvkt4iu7x
 	sed -i "14a $new_dreamFactory" $dir_file_js/jdDreamFactoryShareCodes.js
 	sed -i "15a $new_dreamFactory" $dir_file_js/jdDreamFactoryShareCodes.js
 
+
+	#京喜工厂-金牌厂长
+	new_jdJxStoryShareCodes="xTlGGIL-9JvzV694GpP6Gba8_zk0emJWtyQkQK0otrw=@ijeY8Ncm2eztoQMx4j3LPHswvSkak4Tq3uIuIRzLwcI=@VxcUisKVRxroaObG1_S9WOa-M2odzvod3T2C8i08KQE="
+
+	sed -i "s/'qSDHMwUOz7onHcMyaju4KmdSXWf0dlv7LVnTt1Wzemo='/'xTlGGIL-9JvzV694GpP6Gba8_zk0emJWtyQkQK0otrw=', 'ijeY8Ncm2eztoQMx4j3LPHswvSkak4Tq3uIuIRzLwcI=', 'VxcUisKVRxroaObG1_S9WOa-M2odzvod3T2C8i08KQE='/g" $dir_file_js/jd_jxstory.js
+	sed -i "s/randomCount = 3/randomCount = 0/g" $dir_file_js/jd_jxstory.js
+	sed -i "s/'',/$new_jdJxStoryShareCodes/g" $dir_file_js/jdJxStoryShareCodes.js
+	sed -i "12a $new_jdJxStoryShareCodes" $dir_file_js/jdJxStoryShareCodes.js
+	sed -i "13a $new_jdJxStoryShareCodes" $dir_file_js/jdJxStoryShareCodes.js
+	sed -i "14a $new_jdJxStoryShareCodes" $dir_file_js/jdJxStoryShareCodes.js
+	sed -i "15a $new_jdJxStoryShareCodes" $dir_file_js/jdJxStoryShareCodes.js
 
 	#东东工厂
 	old_jdfactory="\`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj\`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo'"
