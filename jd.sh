dab39c6715f177eab03ae1e4e28bc547bbc31d12
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-02-22 15:01:39 +0800

    继续微调
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -107,7 +107,6 @@ update() {
 
 	if [ ! -d $dir_file/git_clone/lxk0301 ];then
 		git clone -b master git@gitee.com:lxk0301/jd_scripts.git $dir_file/git_clone/lxk0301
-		update
 	else
 		cd $dir_file/git_clone/lxk0301
 		git fetch --all
