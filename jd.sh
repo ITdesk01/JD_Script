3cfa0ff3e038d33d852fb6f38dbe296813d237c8
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-02-27 11:11:26 +0800

    update
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -162,6 +162,7 @@ cat >$dir_file/config/lxk0301_script.txt <<EOF
 	jd_delCoupon.js			#删除优惠券（默认不运行，有需要手动运行）
 	jd_crazy_joy_bonus.js		#监控crazyJoy分红狗(默认不运行，欧皇自己设置定时任务)
 	jd_global_mh.js			#京东国际盲盒
+	jd_unbind.js			#注销京东会员卡
 	getJDCookie.js			#扫二维码获取cookie有效时间可以90天
 	JS_USER_AGENTS.js		#京东极速版UA
 	jd_get_share_code.js		#获取jd所有助力码脚本
@@ -378,7 +379,7 @@ run_07() {
 	$node $dir_file_js/jd_jump-jump.js #母婴-跳一跳
 	$node $dir_file_js/jd_shake.js #超级摇一摇
 	#$node $dir_file_js/jd_unsubscribe.js #取关店铺，没时间要求
-	rm -rf $dir_file_js/jd_unbind.js #注销京东会员卡
+	#$node $dir_file_js/jd_unbind.js #注销京东会员卡
 	$node $dir_file_js/jd_bean_change.js #京豆变更
 	checklog #检测log日志是否有错误并推送
 	echo -e "$green run_07$stop_script $white"
