a7546fd891e73eb2228ff667dec574e7390413a0
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-03-31 19:42:28 +0800

    新增interCenter渠道店铺签到 关注有礼
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -206,14 +206,26 @@ do
 	wget $url2/$script_name -O $dir_file_js/$script_name
 done
 
+url3="https://raw.githubusercontent.com/monk-coder/dust/dust/normal"
+cat >$dir_file/config/monk-coder.txt <<EOF
+	monk_shop_lottery.js #店铺大转盘
+	monk_inter_shop_sign.js #interCenter渠道店铺签到
+	monk_shop_follow_sku.js #关注有礼
+EOF
+
+
+for script_name in `cat $dir_file/config/monk-coder.txt | awk '{print $1}'`
+do
+	wget $url2/$script_name -O $dir_file_js/$script_name
+done
+
 
 	cat $dir_file/config/lxk0301_script.txt > $dir_file/config/collect_script.txt
 	cat $dir_file/config/i-chenzhe_script.txt >> $dir_file/config/collect_script.txt
+	cat $dir_file/config/monk-coder.txt >> $dir_file/config/collect_script.txt
 
 	wget https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_products_detail.js -O $dir_file_js/jx_products_detail.js #京喜工厂商品列表详情
 	wget https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js -O $dir_file_js/jd_try.js #京东试用
-
-	wget https://raw.githubusercontent.com/monk-coder/dust/dust/normal/monk_shop_lottery.js -O $dir_file_js/monk_shop_lottery.js #店铺大转盘
 
 
 
