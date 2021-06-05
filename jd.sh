3f8944d11a1f718f53ddf9248da32b38caee1af1
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-06-06 00:19:12 +0800

    update
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -2158,11 +2158,12 @@ ashou_20210516_jdsgmh="T018v_V1RRgf_VPSJhyb1ACjVQmoaT5kRrbA@T012a0DkmLenrwOACjVQ
 }
 
 zoo_share() {
-	new_zoopk1="'sSKNX-MpqKOJsNu_nZvYV-nCFtEoibN3nsRhO8g77euwQQhVn3QtBsoadt4CFkmn',"
-	new_zoopk2="'sSKNX-MpqKOJsNu-nJyIBnzohu1bg555wuah8sFivgeDWC-K5kCbbW3HgcATcUjo',"
-	new_zoopk3="'sSKNX-MpqKObp_DwnJu2BGGTUTWUfPSxwzGOyQ',"
-	new_zoopk4="'sSKNX-MpqKOXrevjyMWdUScQdhgwySVYfKlCINKPUG9DkQ',"
-	new_zoopk5="'sSKNX-MpqKPQ5rO9mJ3eA9kQEewE3VAI3sBFbJT22o438AhSpqTxO3dqDFxqEXNf',"
+	new_zoopk1="'sSKNX-MpqKOJsNu_nZvYV-nCFtEoibN3nsRhO8g77euwQQhVn3QtBsoadt4CFkmk',"
+	new_zoopk2="'sSKNX-MpqKOJsNu-nJyIBnzohu1bg555wuah8sFivgeDWC-K5kCbbW3HgcATcUjr',"
+	new_zoopk3="'sSKNX-MpqKObp_DwnJu2BGGTUTWUfPSxwzGOyA',"
+	new_zoopk4="'sSKNX-MpqKPQ5rO9mJ3eA9kQEewE3VAI3sBFbJT22o438AhSpqTxO3dqDFxqEXNc',"
+	new_zoopk5="'sSKNX-MpqKOXrevjyMWdUScQdhgwySVYfKlCINKPUG9DkQ',"
+
 
 	sed -i "s/$.inviteList = \[/$.inviteList = \[ \n/g" $dir_file_js/jd_zoo.js
 
