904a74702654c9816be5373af78dabe929998aad
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-06-14 20:28:23 +0800

    更新zoo.js
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -190,7 +190,7 @@ cat >$dir_file/config/tmp/lxk0301_script.txt <<EOF
 	jd_unsubscribe.js		#取关京东店铺和商品
 EOF
 cp  $dir_file/git_clone/lxk0301/activity/jd_unbind.js	$dir_file_js/jd_unbind.js #注销京东会员卡
-cp  $dir_file/JSON/jd_zoo.js 	$dir_file_js/jd_zoo.js		#动物联萌 618活动
+wget https://raw.githubusercontent.com/star261/jd/main/scripts/jd_zoo.js -O 	$dir_file_js/jd_zoo.js		#动物联萌 618活动
 
 for script_name in `cat $dir_file/config/tmp/lxk0301_script.txt | awk '{print $1}'`
 do
