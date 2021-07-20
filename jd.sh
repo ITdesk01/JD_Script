3222b5e49ea816519c973ce50831c8d773567c3c
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-07-20 20:21:27 +0800

    调整挂气球
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -59,7 +59,7 @@ stop_script_time="脚本结束，当前时间：`date "+%Y-%m-%d %H:%M"`"
 script_read=$(cat $dir_file/script_read.txt | grep "我已经阅读脚本说明"  | wc -l)
 
 task() {
-	cron_version="3.46"
+	cron_version="3.47"
 	if [[ `grep -o "JD_Script的定时任务$cron_version" $cron_file |wc -l` == "0" ]]; then
 		echo "不存在计划任务开始设置"
 		task_delete
@@ -94,8 +94,7 @@ cat >>/etc/crontabs/root <<EOF
 0 0 * * * $python3 $dir_file/git_clone/curtinlv_script/getFollowGifts/jd_getFollowGift.py >/tmp/jd_getFollowGift.log #关注有礼#100#
 0 8,15 * * * $python3 $dir_file/git_clone/curtinlv_script/OpenCard/jd_OpenCard.py  >/tmp/jd_OpenCard.log #开卡程序#100#
 59 23 * * 0,1,2,5,6 sleep 59 && $dir_file/jd.sh run_jd_cash >/tmp/jd_cash_exchange.log	#签到领现金兑换#100#
-0 1-23/1 * * * $node $dir_file_js/jd_cfd_loop.js #财富岛挂气球#100#
-59 */1 * * * $dir_file/jd.sh kill_cfd #杀气球#100#
+0 */1 * * * $dir_file/jd.sh cfd_loop #挂气球#100#
 59 23 * * * $python3 $dir_file_js/jd_blueCoin.py >/tmp/jd_blueCoin.log	#东东超市兑换#100#
 45 23 * * * $dir_file/jd.sh kill_ccr #杀掉所有并发进程，为零点准备#100#
 ###########100##########请将其他定时任务放到底下###############
@@ -552,7 +551,19 @@ EOF
 }
 
 cfd_loop() {
-	$node $dir_file_js/jd_cfd_loop.js			#财富岛挂气球
+kill_cfd
+cat >/tmp/jd_tmp/cfd_loop <<EOF
+	jd_cfd_loop.js			#财富岛挂气球
+EOF
+	echo -e "$green cfd_loop$start_script_time $white"
+
+	for i in `cat /tmp/jd_tmp/cfd_loop | grep -v "#.*js" | awk '{print $1}'`
+	do
+		$node $dir_file_js/$i
+		$run_sleep
+	done
+
+	echo -e "$green cfd_loop$stop_script_time $white"
 }
 
 kill_cfd() {
@@ -994,7 +1005,7 @@ concurrent_js_if() {
 			if_ps
 			concurrent_js_clean
 		;;
-		run_01|run_02|run_045|run_08_12_16|run_020|run_10_15_20|run_06_18|run_jd_cash)
+		run_01|run_02|run_045|run_08_12_16|run_020|run_10_15_20|run_06_18|run_jd_cash|cfd_loop)
 			action="$action1"
 			concurrent_js
 			if_ps
@@ -1012,7 +1023,7 @@ concurrent_js_if() {
 			$action1
 			concurrent_js_run_07
 			;;
-			run_01|run_06_18|run_10_15_20|run_03|run_02|run_045|run_08_12_16|run_07|run_030|run_020|run_jd_cash)
+			run_01|run_06_18|run_10_15_20|run_03|run_02|run_045|run_08_12_16|run_07|run_030|run_020|run_jd_cash|cfd_loop)
 			$action1
 			;;
 		esac
@@ -1030,7 +1041,7 @@ concurrent_js_if() {
 			$action2
 			concurrent_js_run_07
 			;;
-			run_01|run_06_18|run_10_15_20|run_03|run_02|run_045|run_08_12_16|run_07|run_020|run_jd_cash)
+			run_01|run_06_18|run_10_15_20|run_03|run_02|run_045|run_08_12_16|run_07|run_020|run_jd_cash|cfd_loop)
 			$action2
 			;;
 		esac
