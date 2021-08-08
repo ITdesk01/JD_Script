0cf6c574b6ceeb3d69abfdb670d13932ffddfcf5
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-08-08 08:59:27 +0800

    jd_opencard_teamBean1_enc.js	#组队瓜分京豆(一次性脚本,默认不运行)
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -282,6 +282,7 @@ cat >$dir_file/config/tmp/Tsukasa007_url.txt <<EOF
 	jd_joypark_task.js		#汪汪乐园每日任务
 	jd_olympic_opencard.js		#一起奔跑 为奥运加油(一次性脚本)
 	jd_opencard_Daddy.js		#8.2-8.12 奶爸盛典 爸气全开(跑完手动领取100豆,只能领一次，所以默认不执行)
+	jd_opencard_teamBean1_enc.js	#组队瓜分京豆(一次性脚本,默认不运行)
 EOF
 
 for script_name in `cat $dir_file/config/tmp/Tsukasa007_url.txt | grep -v "#.*js" | awk '{print $1}'`
