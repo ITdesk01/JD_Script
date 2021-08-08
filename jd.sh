a00eceba74829dd5fa6d853aeb26fe4c3773a6bf
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-08-08 22:52:58 +0800

    删除过期脚本jd_opencard_teamBean1_enc.js
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -283,7 +283,6 @@ cat >$dir_file/config/tmp/Tsukasa007_url.txt <<EOF
 	jd_joypark_task.js		#汪汪乐园每日任务
 	jd_olympic_opencard.js		#一起奔跑 为奥运加油(一次性脚本)
 	jd_opencard_Daddy.js		#8.2-8.12 奶爸盛典 爸气全开(跑完手动领取100豆,只能领一次，所以默认不执行)
-	jd_opencard_teamBean1_enc.js	#组队瓜分京豆(一次性脚本,默认不运行)
 EOF
 
 for script_name in `cat $dir_file/config/tmp/Tsukasa007_url.txt | grep -v "#.*js" | awk '{print $1}'`
@@ -372,6 +371,7 @@ EOF
 
 #删掉过期脚本
 cat >/tmp/del_js.txt <<EOF
+	jd_opencard_teamBean1_enc.js	#组队瓜分京豆(一次性脚本,默认不运行)
 	jd_mb.js			#全民摸冰
 	adolf_superbox.js		#超级盒子
 	jd_jump.js			#跳跳乐瓜分京豆
