7423aa378926db885f5df9d101b32f950f39205d
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-08-10 22:06:24 +0800

    加上＆
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -697,7 +697,7 @@ cat >/tmp/jd_tmp/run_jd_cash <<EOF
 EOF
 	jd_cash_num="20"
 	while [[ ${jd_cash_num} -gt 0 ]]; do
-		$node $dir_file_js/jd_cash_exchange.js
+		$node $dir_file_js/jd_cash_exchange.js &
 		jd_cash_num=$(($jd_cash_num - 1))
 	done
 }
