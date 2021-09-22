1143980e5cfd42384cf6b6a7cde872343585f9cb
ITdesk <38835844+ITdesk01@users.noreply.github.com>
2021-09-22 22:28:50 +0800

    新增开卡gua_opencard36.js

--- jd.sh
+++ jd.sh
@@ -279,6 +279,7 @@ cat >$dir_file/config/tmp/smiek2221_url.txt <<EOF
 	gua_opencard33.js		#开卡默认不运行
 	gua_opencard34.js		#开卡默认不运行
 	gua_opencard35.js		#开卡默认不运行
+	gua_opencard36.js		#开卡默认不运行
 	gua_UnknownTask3.js		#寻找内容鉴赏官
 EOF
 
