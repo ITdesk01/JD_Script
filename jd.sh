4447f15afe8e14af9e657fef728ec159420717ba
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-10-27 14:20:17 +0800

    新增开卡51冲吧
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- jd.sh
+++ jd.sh
@@ -278,6 +278,7 @@ cat >$dir_file/config/tmp/smiek2221_url.txt <<EOF
 	gua_opencard48.js		#开卡默认不运行
 	gua_opencard49.js		#开卡默认不运行
 	gua_opencard50.js		#开卡默认不运行
+	gua_opencard51.js		#开卡默认不运行
 	gua_UnknownTask3.js		#寻找内容鉴赏官
 EOF
 
