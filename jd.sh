5b0fb5733d24a84a0ddab9c63dbe2ee522cc1723
ITdesk <>
2023-01-17 21:00:47 +0800

    更新
    Signed-off-by: ITdesk <>

--- jd.sh
+++ jd.sh
@@ -466,16 +466,11 @@ update_script() {
 	echo -e "${green} update_script$stop_script_time ${white}"
 }
 
-#gua_nhj_Red.js			#年货节red
-if [  -z "$JD_nhj_rebateCode" ];then
-	export JD_nhj_rebateCode="mIUOpeT"
-fi
 
 ccr_run() {
 #这里不会并发
 cat >/tmp/jd_tmp/ccr_run <<EOF
 	jd_bean_sign.js			#京东多合一签到
-	gua_nhj_Red.js			#年货节red
 	jd_ms.js			#秒秒币
 	jd_plus2bean.js                 #plus专属礼
 	jd_supermarket.js		#京东超市游戏
